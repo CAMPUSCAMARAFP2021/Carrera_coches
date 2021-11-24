c76503943017b57ac69189c3e48bf0f81c094aba
luciaCalabrese <lcala419@gmail.com>
2021-11-24 19:08:04 +0100

    gasoil and funcionalities

--- carreras/main.js
+++ carreras/main.js
@@ -3,17 +3,20 @@ class car {
     speedMax;
     acc;
     distance = 0;
+    gasoil = Math.floor((Math.random() * 10) + 1);
+    
 
-    constructor(name,speedMax, acc, brake) {
-       this.name = name;
+    constructor(name, speedMax, acc, brake) {
+        this.name = name;
         this.speedMax = speedMax;
         this.acc = acc;
         this.speed = this.speed;
         this.distance = this.distance;
         this.brake = brake;
         
+
     }
-    
+
     //Poner velocidad max
     accelerate() {
         this.speed = this.speed + this.acc;
@@ -22,7 +25,20 @@ class car {
         if (this.speed === this.speedMax) {
             this.speed = this.speedMax
         }
+      
+        this.gasoil = this.gasoil - 10;
+       
+        if (this.gasoil < 30) {
+            this.repost()
+        }
     }
+
+    repost() {
+        this.instant++;
+        this.gasoil = 100;
+        this.speed = 0;
+    }
+
     braking() {
         this.speed = this.speed - this.brake;
         if (this.speed < 0) {
@@ -33,13 +49,13 @@ class car {
 
 class Circuito {
     name;
-    recorrido = ['r', 'c', 'r', 'c', 'r'];
+    recorrido = ['r', 'c', 'r', 'c', 'r', 'c', 'r'];
 
     constructor(name) {
         this.name = name;
     }
     getStetch(distance) {
-        return this.recorrido [distance % 100 - distance % 100 % this.recorrido.length]
+        return this.recorrido[distance % 100 - distance % 100 % this.recorrido.length]
     }
 }
 class Race {
@@ -47,7 +63,7 @@ class Race {
     circuito = new Circuito("papa")
     distance;
     instant = 0;
-    cars = [new car("Ferrari",200, 5, 5), new car('Bmv',500, 50, 20)];
+    cars = [new car("Ferrari", 100, 7, 2), new car('Bmv', 100, 8, 5)];
     results = [];
 
     constructor(distance, vueltas) {
@@ -55,16 +71,17 @@ class Race {
         this.distance = distance;
     }
 
+
+
     carController(car) {
         const stetch = this.circuito.getStetch(car.distance);
         stetch === 'r' ? car.accelerate() : car.accelerate();
-       
-        
+
     }
 
     getwinner(car) {
         this.results.push(car)
-      }
+    }
 
     start() {
         let timer = setInterval(() => {
@@ -72,21 +89,25 @@ class Race {
             this.instant++;
             this.cars.map(this.carController.bind(this))
             this.cars.map((car) => {
-           
-            if (car.distance >= distancia){
-                this.results.push(car.name)
-                console.log(this.results)
-                 clearInterval(timer);
-            }
-        }
-            )
-        }, 1000);
-        
+
+
+                if (car.distance >= distancia) {
+                    this.results.push(car.name)
+
+
+                    console.log(`El ganador es: ${this.results} con un tiempo de ${this.instant}s`)
+                    console.log(this.results)
+                    clearInterval(timer);
+                }
+            })
+
+        }, 10);
+
     }
 }
 
 
-var race1 = new Race(100, 3);
+var race1 = new Race(1000, 5);
 race1.start();
-console.log(race1.results)
-console.log(`El ganador es:  ${race1.results}`)
+
+
